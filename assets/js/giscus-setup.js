43afd2377771f281b762ed32d69e0082e38eee2a
liangcaixing <136356176+liangcaixing@users.noreply.github.com>
2026-04-18 08:55:05 +0000

    Deploying to gh-pages from @ liangcaixing/liangcaixing.github.io@d1f7c2159cea4b4cd4904ec35e4b5d26968365b4 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "acaihello/liangcaixing.github.io",
+    "data-repo": "liangcaixing/liangcaixing.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
